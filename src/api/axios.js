930365ee6b73b1a410ef0e7d234934a137424998
Amanuel Alemayehu <aman@64318080@gmail.com>
2026-02-20 01:19:22 +0300

    link changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://examwise.up.railway.app/api',
 });
 
 // Add a request interceptor to include the token in headers
